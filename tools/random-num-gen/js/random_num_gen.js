f2b055660f3f194a63ed147b1b7fcedafeb1184b
clstyn <fiorenzacelestyn@gmail.com>
2022-09-10 22:26:31 +0700

    Update random_num_gen.js

--- tools/random-num-gen/js/random_num_gen.js
+++ tools/random-num-gen/js/random_num_gen.js
@@ -1,18 +1,15 @@
 var randomize = document.getElementById("randomize");
 randomize.addEventListener("click", () => {
-  var upper = document.getElementById("upper").value;
-  var lower = document.getElementById("lower").value;
-  // console.log(upper);
-  // console.log(lower);
+  var upper = parseInt(document.getElementById("upper").value);
+  var lower = parseInt(document.getElementById("lower").value);
 
   if (upper > lower){
-    // console.log('true');
     var random_num = Math.floor(Math.random() * (upper - lower) + +lower);
     document.getElementById("output").innerHTML = `${random_num}`;
   }
   else{
-    // console.log('false');
     window.alert("Masukkan angka kedua yang lebih kecil!");
     document.getElementById("lower").value = null;
+    document.getElementById("output").innerHTML = '0';
   };
 });
